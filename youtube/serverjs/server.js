dc6770a50dce8ac85090cffbbc45fea168a96852
Renat <jeingo20@gmail.com>
2022-11-19 15:18:21 +0300

    I got rid of duplicate requests

--- youtube/serverjs/server.js
+++ youtube/serverjs/server.js
@@ -1,7 +1,25 @@
 const http = require('http')
 
+let requestCount = 0
+
 const server = http.createServer((request, response) => {
-    response.write('Hello World!')
+    if (request.url != '/favicon.ico') {
+        requestCount++
+    }
+
+
+    switch (request.url) {
+        case '/students':
+            response.write('Students')
+            break
+        case '/':
+        case '/courses':
+            response.write('FRONT + BACKEND')
+            break
+        default:
+            response.write('Not found 404')
+    }
+    response.write(' - Count: ' + (requestCount))
     response.end()
 })
 
