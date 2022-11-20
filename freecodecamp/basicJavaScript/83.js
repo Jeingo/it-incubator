ed6ed82482aecf4aad8f47a55b9a08564467d86e
Renat <jeingo20@gmail.com>
2022-11-20 22:18:14 +0300

    freecodecamp 83 complete

---
+++ freecodecamp/basicJavaScript/83.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+    "name": "Hex",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Ort", "Set"]
+
+    // Only change code above this line
+};
