324226e5fd207d499be0e9af9c36059316b4b053
Renat <jeingo20@gmail.com>
2022-11-21 13:38:54 +0300

    I started to do favicon.ico

--- youtube/serverjs/server.js
+++ youtube/serverjs/server.js
@@ -1,12 +1,17 @@
 const http = require('http')
+const favicon = require('serve-favicon')
+const finalhandler = require('finalhandler')
+const path = require('path')
 
 let requestCount = 0
 
+let _favicon = favicon(path.join(__dirname, 'favicon.ico'))
+
 const server = http.createServer((request, response) => {
+    let done = finalhandler(request, response)
     if (request.url != '/favicon.ico') {
         requestCount++
     }
-
 
     switch (request.url) {
         case '/students':
@@ -20,7 +25,17 @@ const server = http.createServer((request, response) => {
             response.write('Not found 404')
     }
     response.write(' - Count: ' + (requestCount))
+    _favicon(request, response, function onNext (err) {
+        if (err) return done(err)
+
+        // continue to process the request here, etc.
+
+        response.statusCode = 404
+        response.end('oops')
+    })
     response.end()
+
+
 })
 
 server.listen(3003)
