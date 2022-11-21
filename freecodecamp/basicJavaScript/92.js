9f1088c195646d1c9d3d2174e831ea01119c65cf
Renat <jeingo20@gmail.com>
2022-11-21 13:56:57 +0300

    freecodecamp 92 complete

---
+++ freecodecamp/basicJavaScript/92.js
@@ -0,0 +1,24 @@
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "Michael Jackson",
+        "title": "Who is it",
+        "release_year": 1988,
+        "formats": [
+            "CD",
+            "FLASH"
+        ],
+        "gold": false
+
+    }
+];
