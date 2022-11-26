4bcffd7c013e8f45d927847d864b22ea8ce9669b
Renat <jeingo20@gmail.com>
2022-11-26 11:22:32 +0300

    freecodecamp 13(regexp) complete

---
+++ freecodecamp/regexp/13.js
@@ -0,0 +1,6 @@
+let chewieQuote = "Aaaaaaaaaaaaaaaaaaaaaaagt"
+
+// Only change code below this line
+let chewieRegex = /Aa*/; // Change this line
+// Only change code above this line
+let result = chewieQuote.match(chewieRegex);
