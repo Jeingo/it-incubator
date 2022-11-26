8aef531711eaf2454a74891a5cd761cf7a8dea1f
Renat <jeingo20@gmail.com>
2022-11-26 10:42:36 +0300

    I started learn typescript

---
+++ typescript/types.js
@@ -0,0 +1,19 @@
+var isFetching = true;
+var isLoading = false;
+var int = 42;
+var float = 4.2;
+var num = 3e10;
+var meassage = 'Hello TypeScript';
+var numberArray = [1, 1, 2, 3, 5, 8, 13];
+var numberArray2 = [1, 1, 2, 3, 5, 8, 13];
+var words = ['Hello', 'TypeScript'];
+//Typle
+var contact = ['Renat', 1234567];
+//Any
+var variable = 42;
+variable = 'new';
+//===
+function sayMyName(name) {
+    console.log(name);
+}
+sayMyName('Renat');
---
+++ typescript/types.ts
@@ -0,0 +1,41 @@
+const isFetching: boolean = true
+const isLoading: boolean = false
+
+const int: number = 42
+const float: number = 4.2
+const num: number = 3e10
+
+const meassage: string = 'Hello TypeScript'
+
+const numberArray: number[] = [1, 1, 2, 3, 5, 8, 13]
+const numberArray2: Array<number> = [1, 1, 2, 3, 5, 8, 13]
+
+const words: string[] = ['Hello', 'TypeScript']
+
+//Typle
+const contact: [string, number] = ['Renat', 1234567]
+
+//Any
+let variable: any= 42
+
+variable = 'new'
+
+//===
+
+function sayMyName(name: string): void {
+    console.log(name)
+}
+
+sayMyName('Renat')
+
+//never
+
+function throwError(message: string): never {
+    throw new Error(message)
+}
+
+//Type
+
+type Login = string
+
+const login1: Login = 'admin'
