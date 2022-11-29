ae42fc43b8bb20720d2690b9b919bfd5dbc2db9d
Renat <jeingo20@gmail.com>
2022-11-29 17:40:01 +0300

    codewars 12(js) complete

---
+++ codewars/javascript/12.js
@@ -0,0 +1,17 @@
+function updateLight(current) {
+    switch (current) {
+        case 'green': {
+            return 'yellow'
+            break
+        }
+        case 'yellow': {
+            return 'red'
+            break
+        }
+        default : {
+            return 'green'
+            break
+        }
+    }
+
+}
