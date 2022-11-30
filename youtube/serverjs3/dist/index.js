90ff4e9580ca95936a0e5913aec26a974009e843
Renat <jeingo20@gmail.com>
2022-11-30 17:32:15 +0300

    i did lesson 09 backend

--- youtube/serverjs3/dist/index.js
+++ youtube/serverjs3/dist/index.js
@@ -6,20 +6,55 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
-app.get('/', (req, res) => {
-    const a = 4;
-    if (a > 5) {
-        res.send('OK');
+const jsonBody = express_1.default.json();
+app.use(jsonBody);
+const db = {
+    courses: [
+        { id: 1, title: 'front-end' },
+        { id: 2, title: 'back-end' },
+        { id: 3, title: 'test' },
+        { id: 4, title: 'fullstack' }
+    ]
+};
+app.get('/courses', (req, res) => {
+    let foundCourses = db.courses;
+    if (req.query.title) {
+        foundCourses = foundCourses.filter(c => c.title.indexOf(req.query.title) > -1);
     }
-    else {
-        res.send('Hello World!!!!');
+    res.json(foundCourses);
+});
+app.get('/courses/:id', (req, res) => {
+    const foundCourse = db.courses.find(c => c.id === +req.params.id);
+    if (!foundCourse) {
+        res.sendStatus(404);
+        return;
     }
+    res.json(foundCourse);
 });
-app.get('/samurais', (req, res) => {
-    res.send('Hello Samurais!');
+app.post('/courses', (req, res) => {
+    if (!req.body.title) {
+        res.sendStatus(400);
+        return;
+    }
+    const createdCourse = {
+        id: +(new Date()),
+        title: req.body.title
+    };
+    db.courses.push(createdCourse);
+    res.status(201).json(createdCourse);
 });
-app.post('/samurais', (req, res) => {
-    res.send('We created Samurai');
+app.delete('/courses/:id', (req, res) => {
+    db.courses = db.courses.filter(c => c.id !== +req.params.id);
+    res.sendStatus(204);
+});
+app.put('/courses/:id', (req, res) => {
+    const foundCourse = db.courses.find(c => c.id === +req.params.id);
+    if (!foundCourse) {
+        res.sendStatus(404);
+        return;
+    }
+    foundCourse.title = req.body.title;
+    res.sendStatus(204);
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- youtube/serverjs3/src/index.ts
+++ youtube/serverjs3/src/index.ts
@@ -2,21 +2,63 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    const a = 4
-    if (a > 5) {
-        res.send('OK')
-    } else {
-        res.send('Hello World!!!!')
+const jsonBody = express.json()
+app.use(jsonBody)
+
+const db = {
+    courses: [
+        {id: 1, title: 'front-end'},
+        {id: 2, title: 'back-end'},
+        {id: 3, title: 'test'},
+        {id: 4, title: 'fullstack'}
+    ]
+}
+
+app.get('/courses', (req, res) => {
+    let foundCourses = db.courses
+    if (req.query.title) {
+        foundCourses = foundCourses.filter(c => c.title.indexOf(req.query.title as string) > -1)
     }
+    res.json(foundCourses)
 })
 
-app.get('/samurais', (req, res) => {
-    res.send('Hello Samurais!')
+app.get('/courses/:id', (req, res) => {
+    const foundCourse = db.courses.find(c => c.id === +req.params.id);
+    if (!foundCourse) {
+        res.sendStatus(404)
+        return
+    }
+
+    res.json(foundCourse)
 })
 
-app.post('/samurais', (req, res) => {
-    res.send('We created Samurai')
+app.post('/courses', (req, res) => {
+    if (!req.body.title) {
+        res.sendStatus(400)
+        return
+    }
+    const createdCourse = {
+        id: +(new Date()),
+        title: req.body.title
+    }
+    db.courses.push(createdCourse)
+    res.status(201).json(createdCourse)
+})
+
+app.delete('/courses/:id', (req, res) => {
+    db.courses = db.courses.filter(c => c.id !== +req.params.id)
+
+    res.sendStatus(204)
+})
+
+app.put('/courses/:id', (req, res) => {
+    const foundCourse = db.courses.find(c => c.id === +req.params.id);
+    if (!foundCourse) {
+        res.sendStatus(404)
+        return
+    }
+    foundCourse.title = req.body.title
+    res.sendStatus(204)
 })
 
 app.listen(port, () => {
