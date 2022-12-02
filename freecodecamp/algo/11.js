0f1af0573b3b34da12f94bd88aa1a6665fe47c7d
Renat <jeingo20@gmail.com>
2022-12-02 19:04:28 +0300

    freecodecamp 11(algorithm) complete

---
+++ freecodecamp/algo/11.js
@@ -0,0 +1,13 @@
+function titleCase(str) {
+    const newTitle = str.split(" ");
+    const updatedTitle = [];
+    for (let st in newTitle) {
+        updatedTitle[st] = newTitle[st][0].toUpperCase() + newTitle[st].slice(1).toLowerCase();
+    }
+    return updatedTitle.join(" ");
+}
+
+
+let res = titleCase("I'm a little tea pot");
+
+console.log(res)
