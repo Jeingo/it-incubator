976da76616b4cf3798a1afd2a13e96b019d0a476
Renat <jeingo20@gmail.com>
2022-12-13 14:18:53 +0300

    freecodecamp 12(func programming) complete

---
+++ freecodecamp/funcProg/12.js
@@ -0,0 +1,10 @@
+function nonMutatingSplice(cities) {
+    // Only change code below this line
+    const res = cities.slice(0,3)
+    return res
+
+    // Only change code above this line
+}
+
+const inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+nonMutatingSplice(inputCities);
