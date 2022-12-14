0a6a709e477a66abdfffe3c52d99683514c73550
Renat <jeingo20@gmail.com>
2022-12-14 17:16:12 +0300

    freecodecamp 19(func programming) complete

---
+++ freecodecamp/funcProg/19.js
@@ -0,0 +1,8 @@
+function splitify(str) {
+    // Only change code below this line
+    const arr = str.split(/\W/)
+    return arr
+    // Only change code above this line
+}
+
+splitify("Hello World,I-am code");
